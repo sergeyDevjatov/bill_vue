19ac75c4eb6ceef99466d66e5e49ca45abd8cfb3
Сергей Девятов <somova25@mail.ru>
2017-11-15 00:20:20 +0300

    Fix bug with saving field after removing position

--- src/VClickOutsideEvent.js
+++ src/VClickOutsideEvent.js
@@ -5,30 +5,35 @@ Source: https://jsfiddle.net/Linusborg/Lx49LaL8/
 
  */
 
-export default {
-    bind: function(el, binding, vNode) {
-        // Provided expression must evaluate to a function.
-        if (typeof binding.value !== 'function') {
-            const compName = vNode.context.name;
-            let warn = `[Vue-click-outside:] provided expression '${binding.expression}' is not a function, but has to be`;
-            if (compName) { warn += `Found in component '${compName}'` }
-
-            console.warn(warn)
+function addHandlerFunc(el, binding, vNode) {
+    // Provided expression must evaluate to a function.
+    if (typeof binding.value !== 'function') {
+        const compName = vNode.context.name;
+        let warn = `[Vue-click-outside:] provided expression '${binding.expression}' is not a function, but has to be`;
+        if (compName) {
+            warn += `Found in component '${compName}'`
         }
-        // Define Handler and cache it on the element
-        const bubble = binding.modifiers.bubble
-        const handler = (e) => {
-            if (bubble || (!el.contains(e.target) && el !== e.target)) {
-                binding.value(e)
-            }
-        };
-        el.__vueClickOutside__ = handler;
 
-        // add Event Listeners
-        document.addEventListener('click', handler)
-    },
+        console.warn(warn)
+    }
+    // Define Handler and cache it on the element
+    const bubble = binding.modifiers.bubble;
+    const handler = (e) => {
+        if (bubble || (!el.contains(e.target) && el !== e.target)) {
+            binding.value(e)
+        }
+    };
+    el.__vueClickOutside__ = handler;
 
-    unbind: function(el, binding) {
+    // add Event Listeners
+    document.addEventListener('click', handler)
+}
+
+export default {
+    bind: addHandlerFunc,
+    update: addHandlerFunc,
+
+    unbind: function(el) {
         // Remove Event Listeners
         document.removeEventListener('click', el.__vueClickOutside__);
         el.__vueClickOutside__ = null;
